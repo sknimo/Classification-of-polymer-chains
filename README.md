c7b5ef8de8cdaf3d197b3a1c35ca59d7d917eff0
Shadrach Kwakye-Nimo <kwakyenimo@gmail.com>
2023-07-03 15:27:10 -0400

    add readme file

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Peak-deconvolution-using-clustering
+# Classification of polymer chain based on their production sites
+---
+![Image of polymer chains](https://augustrs.com/wp-content/uploads/2020/02/Polymer-Chain_1.jpg)
+<sub><sup>Image source: https://augustrs.com/wp-content/uploads/2020/02/Polymer-Chain_1.jpg</sup></sub>
+
 Making use of unsupervised learning to perform peak deconvolution on molecular weight distributions of polymers
+
+### Table of Contents
+- [**Introduction**](#introduction)
+- [**Project structure**](#project-structure)
+- [**Dataset**](#dataset)
+- [**Key insight**](#key-insights)
+- [**Tools used in this project**](#tools-used-in-this-project)
+
+## Introduction
+---
+>A classification algorithm was used to segment polymer chains into production site based on their chain length. The motivation of this project is to identify the various clusters in a molecular weight distribution of polymer chains and use such information to perform peak deconvolutions. Traditionally peak deconvolution is done using commercial softwares which require the user to be able to manually indicate the peak locations prior to the deconvolution. But with the production by day of more complex materials a distribution that may seem to have two cluster could actually contain more which are invisible to the eye. This program uses machine learning to identify all clusters and subsequently performs a peak deconvolution.
+
+### Project structure
+* the folder 'reports' contains:
+    * the executive summary
+    * notes on data preprocessing
+* all codes can be found in the folder 'src'
+
+## Dataset
+---
+The dataset was digitized from a paper published by [*kwakye-Nimo et al.(2022)*](https://link.springer.com/article/10.1007/s00397-022-01340-5)
+
+
+## Key insights
+---
+The subpopulations in a molecular weight distribution can be identified using Machine learning. With knowledge of the sub-population characteristics and optimization routine can be subsequently used to effectively perform peak deconvolution. This method removes the need for the user to know ahead of time the characteristics of the peaks he is seeking to identify.
+
+
+![Image result](reports/figures/deconvoluted.png)
+
+## Tools used in this project
+---
+* Packages: Anaconda
+* Libraries: Pandas, Numpy, Scipy, Matplotlib, Seaborn
+* Web applications: Jupyter notebook
+* Programming languages: Python 3, Latex
+
--- src/graphs.py
+++ src/graphs.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 plt.style.use('ggplot')
 
-# plotting the error
-# error = pd.read_csv('../data/processed/errors.csv')
+plotting the error
+error = pd.read_csv('../data/processed/errors.csv')
 
-# fig = plt.figure(figsize=(4, 3))
-# plt.plot(error.n_comps, error.error, 'b-*', lw=3)
-# plt.xlabel('no. of clusters', fontsize=14)
-# plt.ylabel('$\Delta \; AUC$', fontsize=14)
-# plt.savefig('visualization/error_curve.png', bbox_inches='tight', dpi=300)
-# plt.show()
+fig = plt.figure(figsize=(4, 3))
+plt.plot(error.n_comps, error.error, 'b-*', lw=3)
+plt.xlabel('no. of clusters', fontsize=14)
+plt.ylabel('$\Delta \; AUC$', fontsize=14)
+plt.savefig('visualization/error_curve.png', bbox_inches='tight', dpi=300)
+plt.show()
 
 # # plotting the distributions
 wb = pd.ExcelFile('../data/processed/distributions.xlsx')
@@ -19,28 +19,28 @@ ncomps = ['nComps3', 'nComps4']
 # comparing the predictions based on number of clusters
 
 
-# def plt_distros(wb, ncomps):
+def plt_distros(wb, ncomps):
 
-#     fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-#     n = 3
-#     for sheet in range(len(ncomps)):
-#         df = wb.parse(ncomps[sheet])
-#         axs[sheet].plot(df.iloc[:, 0], df.iloc[:, 1], 'k:', lw=2)
-#         for curve in range(2, len(df.columns)):
-#             axs[sheet].plot(df.iloc[:, 0], df.iloc[:, curve],
-#                             ls='-.', lw=3, label=f'cluster {curve-1}')
-#         axs[sheet].set_title(f'no. of clusters = {n}')
-#         axs[sheet].legend()
-#         n += 1
-#     fig.supxlabel('$Log \; M$', fontsize=14)
-#     fig.supylabel('$dw/dLogM$', fontsize=14)
-#     plt.savefig('visualization/comparison_nclusters.png',
-#                 bbox_inches='tight', dpi=300)
-#     plt.show()
-#     return None
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    n = 3
+    for sheet in range(len(ncomps)):
+        df = wb.parse(ncomps[sheet])
+        axs[sheet].plot(df.iloc[:, 0], df.iloc[:, 1], 'k:', lw=2)
+        for curve in range(2, len(df.columns)):
+            axs[sheet].plot(df.iloc[:, 0], df.iloc[:, curve],
+                            ls='-.', lw=3, label=f'cluster {curve-1}')
+        axs[sheet].set_title(f'no. of clusters = {n}')
+        axs[sheet].legend()
+        n += 1
+    fig.supxlabel('$Log \; M$', fontsize=14)
+    fig.supylabel('$dw/dLogM$', fontsize=14)
+    plt.savefig('visualization/comparison_nclusters.png',
+                bbox_inches='tight', dpi=300)
+    plt.show()
+    return None
 
 
-# plt_distros(wb, ncomps)
+plt_distros(wb, ncomps)
 
 
 df_3comp = wb.parse(sheet_name='nComps3')
@@ -54,6 +54,7 @@ ax.plot(df_3comp.iloc[:, 0], df_3comp.iloc[:, 1],
         'k', lw=2, label='original distribution')
 ax.plot(df_3comp.iloc[:, 0], df_3comp.overall,
         'r-.', lw=2, label='sum of clusters')
+# grouping them into subpopulation
 lmw = df_curves[['curve1', 'curve2']].sum(axis=1)
 hmw = df_curves['curve3']
 
